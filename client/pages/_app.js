70fa29dc8a29be0222ca7368d10b7ed6bd82ee5b
gussti <gusaranciba@gmail.com>
2023-03-30 22:16:55 -0300

    faltaba guardar cambios en _app.js

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -9,6 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 export default function MyApp({ Component, pageProps }) {
   const [auth, setAuth] = useState(undefined);
+  const [reloadUser, setReloadUser] = useState(false);
 
   useEffect(() => {
     const token = getToken();
@@ -20,7 +21,8 @@ export default function MyApp({ Component, pageProps }) {
     } else {
       setAuth(null);
     }
-  }, []);
+    setReloadUser(false);
+  }, [reloadUser]);
 
   const login = (token) => {
     setToken(token);
@@ -34,10 +36,12 @@ export default function MyApp({ Component, pageProps }) {
       auth,
       login,
       logout: () => null,
-      setReloadUser: () => null,
+      setReloadUser,
     }),
     [auth]
   );
+
+  if (auth === undefined) return null; // porque esta comprobando aun si el user está logueado
 
   return (
     <AuthContext.Provider value={authData}>
