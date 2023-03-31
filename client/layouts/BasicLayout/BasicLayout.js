8ecc61776e8e31a0638ddf08eed2a7e10fa3a1c4
gussti <gusaranciba@gmail.com>
2023-03-31 12:09:08 -0300

    Creando la página de usuario, implementando npm i classnames

--- client/layouts/BasicLayout/BasicLayout.js
+++ client/layouts/BasicLayout/BasicLayout.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 //import "./BasicLayout.scss" En React va peefeto, pero en NextJS nop(se duplican css)
+import classNames from "classnames";
 import Header from "../../components/Header";
 
 export default function BasicLayout(props) {
-  const { children } = props;
+  const { children, className } = props;
+
+  //className="basic-layout"
+
   return (
-    <Container fluid className="basic-layout">
+    <Container
+      fluid
+      className={classNames("basic-layout", {
+        [className]: className,
+      })}
+    >
       <Header />
       <Container className="content">{children}</Container>
     </Container>
