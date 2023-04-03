0e46ad75d28058bd003d4576cdd5701acf913652
gussti <gusaranciba@gmail.com>
2023-04-03 10:54:57 -0300

    Bloqueo de paginas a usuarios deslogueados y estilos

--- client/pages/account.js
+++ client/pages/account.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import BasicLayout from "../layouts/BasicLayout";
+import useAuth from "../hooks/useAuth"; // De acá obtenemos si el user esta logged
+import { getMeApi } from "../api/user";
 
 export default function Account() {
+  const [user, setUser] = useState(undefined);
+  const { auth, logout } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    (async () => {
+      const response = getMeApi(logout);
+    })();
+  }, []);
+
   return (
     <BasicLayout className="account">
-      <h1>Estamos en mi cuenta</h1>
+      <Configuration />
     </BasicLayout>
+  );
+}
+
+function Configuration() {
+  return (
+    <div className="acconut__configuration">
+      <div className="title">Configuración</div>
+      <div className="data">Formularios de configuración</div>
+    </div>
   );
 }
