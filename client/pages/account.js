650c198405db5bc6e71103566a62ad865c80064a
gussti <gusaranciba@gmail.com>
2023-04-04 22:50:07 -0300

    Creamos el formulario de cambio de contraseña

--- client/pages/account.js
+++ client/pages/account.js
@@ -5,6 +5,7 @@ import useAuth from "../hooks/useAuth"; // De acá obtenemos si el user esta log
 import { getMeApi } from "../api/user";
 import ChangeNameForm from "../components/Account/ChangeNameForm";
 import ChangeEmailForm from "../components/Account/ChangeEmailForm";
+import ChangePasswordForm from '../components/Account/ChangePasswordForm';
 
 export default function Account() {
   const [user, setUser] = useState(undefined);
@@ -52,6 +53,9 @@ function Configuration(props) {
           logout={logout}
           setReloadUser={setReloadUser}
         />
+        <ChangePasswordForm
+          user={user}
+          logout={logout}
       </div>
     </div>
   );
